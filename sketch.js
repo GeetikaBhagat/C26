62c3a9b9ce87ca9cc2f1322099f392f16f79cc84
geetika <geetu911@gmail.com>
2020-10-26 15:13:07 +0530

    Made changes to sketch

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var box7;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +36,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    box7 = new Box(300,300,50,50);
 }
 
 function draw(){
